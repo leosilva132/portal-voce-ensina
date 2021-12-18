1d65180237fd2f984d96df6377c97170f5dc9a7f
LeoSilva <95375911+leosilva132@users.noreply.github.com>
2021-12-18 17:25:04 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+import initAnimacaoScroll from './modules/anima-scroll.js';
+import initAnimaNumeros from './modules/anima-numeros.js';
+
+
+
+
+initAnimacaoScroll();
+initAnimaNumeros();
+
+
+
+
+
+
+
