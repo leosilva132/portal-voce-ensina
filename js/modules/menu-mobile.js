f83acc865ba863b7c3894499aa76dc0255055593
LeoSilva <95375911+leosilva132@users.noreply.github.com>
2021-12-19 09:50:35 -0300

    Add files via upload

---
+++ js/modules/menu-mobile.js
@@ -0,0 +1,23 @@
+import outsideClick from './outsideClick.js';
+
+export default function initMenuMobile() {
+  const menuButton = document.querySelector('[data-menu="button"]');
+  const menuList = document.querySelector('[data-menu="list"]');
+  const menuIcon = document.querySelector('.hb-menu');
+  const eventos = ['click', 'touchstart'];
+  console.log('[data-menu="list"]');
+  if(menuButton) {
+  function openMenu(event) {
+    menuList.classList.add('ativa');
+    menuButton.classList.add('ativa');
+    menuIcon.classList.add('ativa');
+    outsideClick(menuList, eventos, () => {
+  
+      menuList.classList.remove('ativa');
+      menuButton.classList.remove('ativa');
+      menuIcon.classList.remove('ativa');
+    })
+  }
+  eventos.forEach(evento => menuButton.addEventListener(evento, openMenu));
+  }
+}
